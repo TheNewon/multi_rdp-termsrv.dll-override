5aed2ab7ece49505f7227e8e507f8d3b6b35063e
TheNewon <47224047+TheNewon@users.noreply.github.com>
2020-07-23 17:05:38 +0200

    first submit
    copy file and dll to downloaded folder then run py or exe file.

--- temserv(multi_rdp).py
+++ temserv(multi_rdp).py
@@ -29,4 +29,5 @@ if x!=y:
             newFileByteArray = bytearray(content_BA)
             fh.write(newFileByteArray)
             
-print('-----------------saved---------------')            
+print('-----------------saved---------------')
+input('press enter to exit')
